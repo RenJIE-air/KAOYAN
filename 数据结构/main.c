ac605d8263ee81e44971ce8cc4ec6ebb7b0866a4
Air75 <1916790564@qq.com>
2021-04-06 19:53:01 +0800

    数据结构学习笔记2021.4.6

---
+++ 数据结构/main.c
@@ -0,0 +1,12 @@
+void LoveYou(int n){
+    int i = 1;
+    while (i<=n)
+    {
+        i++;
+        printf("I love you %d\n",i);
+    }
+    printf("I love you more than %d\n",n);
+}
+void main(){
+    LoveYou(3000);
+}
